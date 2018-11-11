29bba7c44a5a14c6c38157d1c300a15fe8d7d7bc
Nikita Koren <nikita.koren@gmail.com>
2018-11-11 08:56:30 -0800

    Integrate node with Stackoverflow

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import compression from 'compression'
+import request from 'request'
 
 const  app = express()
 
@@ -10,10 +11,30 @@ app.use(morgan('dev'));
 app.use(compression())
 app.use(cors())
 
-app.get('/', function (req, res) {
-  res.send('hello world')
+
+const getUserStackOverflowInfo = () => {
+  request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    console.log(body.url);
+    console.log(body.explanation);
+  });
+}
+
+app.get('/status', function (req, res) {
+  res.send('Running CognEOS Node')
+})
+
+app.get('/check-stackoverflow-activity/:userId', function (req, res) {
+  getUserStackOverflowInfo()
+  res.send(`Check activity of a user with id: 0`)
 })
 
 http.createServer(app).listen(8000);
+
+request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  console.log(body.url);
+  console.log(body.explanation);
+});
 
 module.exports = app;  
