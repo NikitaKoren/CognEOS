2b273f6396283c2ae1f2701526d23bd878cd9b66
Nikita Koren <nikita.koren@gmail.com>
2018-11-11 13:08:11 -0800

    Update Data in EOS

--- blockchain/cogneos/datascript.sh
+++ blockchain/cogneos/datascript.sh
@@ -1,27 +1,27 @@
-cleos push action cogneos deletecourse '["cogneos", 0]' -p congeos@active
-cleos push action cogneos deletecourse '["cogneos", 1]' -p congeos@active
-cleos push action cogneos deletecourse '["cogneos", 2]' -p congeos@active
-cleos push action cogneos deletecourse '["cogneos", 3]' -p congeos@active
-cleos push action cogneos deletecourse '["cogneos", 4]' -p congeos@active
-cleos push action cogneos deletecourse '["cogneos", 5]' -p congeos@active
-cleos push action cogneos deletecourse '["cogneos", 6]' -p congeos@active
-cleos push action cogneos deletecourse '["cogneos", 7]' -p congeos@active
-cleos push action cogneos deletecourse '["cogneos", 8]' -p congeos@active
-cleos push action cogneos deletecourse '["cogneos", 9]' -p congeos@active
-cleos push action cogneos deletecourse '["cogneos", 10]' -p congeos@active
+cleos push action cogneos deletecourse '["cogneos", 0]' -p cogneos@active
+cleos push action cogneos deletecourse '["cogneos", 1]' -p cogneos@active
+cleos push action cogneos deletecourse '["cogneos", 2]' -p cogneos@active
+cleos push action cogneos deletecourse '["cogneos", 3]' -p cogneos@active
+cleos push action cogneos deletecourse '["cogneos", 4]' -p cogneos@active
+cleos push action cogneos deletecourse '["cogneos", 5]' -p cogneos@active
+cleos push action cogneos deletecourse '["cogneos", 6]' -p cogneos@active
+cleos push action cogneos deletecourse '["cogneos", 7]' -p cogneos@active
+cleos push action cogneos deletecourse '["cogneos", 8]' -p cogneos@active
+cleos push action cogneos deletecourse '["cogneos", 9]' -p cogneos@active
+cleos push action cogneos deletecourse '["cogneos", 10]' -p cogneos@active
 
 
-cleos push action cogneos delstdcourse '["cogneos", 0]' -p congeos@active
-cleos push action cogneos delstdcourse '["cogneos", 1]' -p congeos@active
-cleos push action cogneos delstdcourse '["cogneos", 2]' -p congeos@active
-cleos push action cogneos delstdcourse '["cogneos", 3]' -p congeos@active
-cleos push action cogneos delstdcourse '["cogneos", 4]' -p congeos@active
-cleos push action cogneos delstdcourse '["cogneos", 5]' -p congeos@active
-cleos push action cogneos delstdcourse '["cogneos", 6]' -p congeos@active
-cleos push action cogneos delstdcourse '["cogneos", 7]' -p congeos@active
-cleos push action cogneos delstdcourse '["cogneos", 8]' -p congeos@active
-cleos push action cogneos delstdcourse '["cogneos", 9]' -p congeos@active
-cleos push action cogneos delstdcourse '["cogneos", 10]' -p congeos@active
+cleos push action cogneos delstdcourse '["cogneos", 0]' -p cogneos@active
+cleos push action cogneos delstdcourse '["cogneos", 1]' -p cogneos@active
+cleos push action cogneos delstdcourse '["cogneos", 2]' -p cogneos@active
+cleos push action cogneos delstdcourse '["cogneos", 3]' -p cogneos@active
+cleos push action cogneos delstdcourse '["cogneos", 4]' -p cogneos@active
+cleos push action cogneos delstdcourse '["cogneos", 5]' -p cogneos@active
+cleos push action cogneos delstdcourse '["cogneos", 6]' -p cogneos@active
+cleos push action cogneos delstdcourse '["cogneos", 7]' -p cogneos@active
+cleos push action cogneos delstdcourse '["cogneos", 8]' -p cogneos@active
+cleos push action cogneos delstdcourse '["cogneos", 9]' -p cogneos@active
+cleos push action cogneos delstdcourse '["cogneos", 10]' -p cogneos@active
 
 cleos push action cogneos addcourse '["cogneos", 0, "EOS Fundamentals", "EOS Fundamentals", 40, 50]' -p cogneos@active
 cleos push action cogneos addcourse '["cogneos", 0, "C++ Fundamentals", "C++ Fundamentals", 50, 40]' -p cogneos@active
