4afd3b72884b74f0f17695f97b006c127ae4a464
Nikita Koren <nikita.koren@gmail.com>
2018-11-10 20:09:55 -0800

    Setup react redux app and add templates

---
+++ template/src/containers/login/login.container.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class LoginContainer extends Component {
+  redner() {
+    return (
+      <div className='login'>
+        Login Page
+      </div>
+    );
+  }
+};
+
+export default LoginContainer
